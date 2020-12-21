0fbfc6bda59378d2cdc6fa52dbd848b0fd495b0b
Jake Congdon <Mystiick@outlook.com>
2020-12-20 19:13:57 -0600

    Begin reading recipe file

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow, ipcMain} = require('electron');
+const fs = require('fs');
+const path = require('path');
+const { error } = require('console');
+
+let recipes;
+let items;
 
 function createWindow () {
   const win = new BrowserWindow({
@@ -9,7 +15,9 @@ function createWindow () {
     }
   });
 
-  win.loadFile('./src/index.html');
+  win.loadURL("file://" + __dirname + "/src/index.html");
+
+  win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow);
@@ -25,3 +33,77 @@ app.on('activate', () => {
     createWindow();
   }
 });
+
+// TODO: Move into its own IPC Main file?
+ipcMain.on('load-data-start', () => {
+  console.log("starting load");
+  let recipePath = path.join(__dirname, 'data/Recipe.csv');
+
+  fs.exists(recipePath, (exists) => {
+    
+    if (exists){
+      console.log('file exists');
+      fs.readFile(recipePath, 'utf-8', handleRecipeLoad);
+    }
+    else {
+      ipcMain.send('load-data-finish', undefined);
+    }
+
+  });
+
+  // let data = fs.readFileSync("./data/data.json", 'utf8');
+  // // let obj = JSON.parse(data);
+
+  // document.getElementById("loading").innerText = "Loaded";
+
+  // // document.getElementById("testResult").innerText = obj.test;
+  // // document.getElementById("wasSuccess").innerText = obj.success;
+  // document.getElementById("contents").style = "";
+});
+
+// TODO: Move into its own Recipe file
+function handleRecipeLoad(err, data){
+  if (err) {
+    alert(err.message);
+  }
+  else {
+    
+    let lines = data.split('\n');
+    let recipes = [];
+
+    console.log(lines[3]);
+
+    for (let i = 3; i < lines.length - 1; i++){
+      let columns = lines[i].split(',');
+      if (!columns[0]) continue;
+
+      recipes.push({
+        "key": columns[0],
+        "number": columns[1],
+        "craftType": cleanString(columns[2]),
+        "recipeLevelTable": cleanString(columns[3]),
+        "itemName": cleanString(columns[4]),
+        "craftedAmount": columns[5],
+        "requiredItems": parseRecipe(columns)
+      });
+    }
+
+    console.log(recipes[1]);
+  }
+}
+
+function cleanString(input) {
+  if (input.charAt(0) === "\""){
+    input = input.substring(1);
+  }
+
+  if (input.charAt(input.length - 1) === "\""){
+    input = input.substring(0, input.length - 1);
+  }
+
+  return input;
+}
+
+function parseRecipe() {
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 const fs = require('fs');
+const { ipcRenderer } = require('electron');
 
-document.addEventListener("DOMContentLoaded", onLoad);
+document.addEventListener('DOMContentLoaded', onLoad);
 
 function onLoad() {
-    
-
-    let data = fs.readFileSync("./src/data.json", 'utf8');
-    let obj = JSON.parse(data);
-
-    document.getElementById("loading").innerText = "Loaded";
-
-    document.getElementById("testResult").innerText = obj.test;
-    document.getElementById("wasSuccess").innerText = obj.success;
-    document.getElementById("contents").style = "";
+    ipcRenderer.send('load-data-start');
 }
