73c520133fb65060702d29a73fb201ecdf963c20
Jake Congdon <Mystiick@outlook.com>
2021-01-07 21:22:47 -0600

    Add mappings file

---
+++ src/mappings.js
@@ -0,0 +1,23 @@
+module.exports = {
+    test: function() {
+        console.log("Test success");
+    },
+    recipe: {
+        key: 0,
+        number: 1,
+        craftType: 2,
+        recipeLevel: 3,
+        itemName: 4,
+        craftedAmount: 5,
+        items: [
+            { name: 6, quantity: 7},
+            { name: 8, quantity: 9},
+            { name: 10, quantity: 11},
+            { name: 12, quantity: 13},
+            { name: 14, quantity: 15},
+            { name: 16, quantity: 17},
+            { name: 18, quantity: 19},
+            { name: 20, quantity: 21}
+        ]
+    }
+}
